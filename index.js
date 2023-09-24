2bd8ab2a5b7443a7f1de6648d85deb8db32af8b1
maddie <lee.maddie@gmail.com>
2023-09-24 15:39:42 -0400

    Loop through results array using forEach

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
 const { nextISSTimesForMyLocation } = require ('./iss');
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:', ip);
-// });
-
-// fetchCoordsByIP('66.234.62.74', (error, data) => {
-//     if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned coordinates:', data);
-// });
-
-// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, data) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned data:', data);
-// });
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
@@ -36,5 +6,7 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return;
   }
 
-  console.log('It worked! Returned pass times:', passTimes);
+  passTimes.forEach((passTime) => {
+    console.log(passTime);
+  })
 });

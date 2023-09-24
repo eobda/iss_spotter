1b75166e1f37781180402be361c4ef005b285bfa
maddie <lee.maddie@gmail.com>
2023-09-24 15:49:58 -0400

    Separate console logging into its own function

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const { nextISSTimesForMyLocation } = require ('./iss');
 
+const printPassTimes = function(passTimes) {
+  passTimes.forEach((passTime) => {
+    // convert epoch time date to human readable date
+    const date = new Date(passTime.risetime *1000);
+    const duration = passTime.duration;
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
+  });
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  passTimes.forEach((passTime) => {
-    // convert epoch time date to human readable date
-    const date = new Date(passTime.risetime *1000);
-    console.log(`Next pass at ${date} for ${passTime.duration} seconds!`);
-  })
+  printPassTimes(passTimes);
 });

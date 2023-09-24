724f2906b6c771ff5568351cfd6c70d698f34262
maddie <lee.maddie@gmail.com>
2023-09-24 12:42:30 -0400

    Refactor code for readability

--- iss.js
+++ iss.js
@@ -12,19 +12,15 @@ const request = require('request');
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
+    if (error) return callback(error, null);
     
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
       return;
     }
     
-    const data = JSON.parse(body);
-    callback(null, data.ip);
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
     return;
   });
 };

e45d694becd0b7af463151f92005ac08ef5273c3
maddie <lee.maddie@gmail.com>
2023-09-24 13:47:58 -0400

    Declare fetchISSFlyOverTimes function

--- iss.js
+++ iss.js
@@ -45,4 +45,18 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+};
+
 module.exports = { fetchMyIP, fetchCoordsByIP };

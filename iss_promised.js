69c2b57ba9732c096f4d86220f583949ce09bb81
maddie <lee.maddie@gmail.com>
2023-09-24 16:23:08 -0400

    Insert comment for function

--- iss_promised.js
+++ iss_promised.js
@@ -1,8 +1,11 @@
 const request = require('request-promise-native');
 
+/*
+Requests user's ip address from https://www.ipify.org/
+- Input: None
+- Returns: Promise of request for ip data, returned as JSON string
+*/
 const fetchMyIP = function() {
-  // Fetch the IP address from the API using request
-  // Return the promise that is returned by request
   return request('https://api.ipify.org/?format=json');
 };
 

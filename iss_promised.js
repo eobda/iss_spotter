39c146a3ce625085a40314e33c977fd376359fe2
maddie <lee.maddie@gmail.com>
2023-09-24 16:16:04 -0400

    Implement fetchMyIP using promises

--- iss_promised.js
+++ iss_promised.js
@@ -3,6 +3,8 @@ const request = require('request-promise-native');
 const fetchMyIP = function() {
   // Fetch the IP address from the API using request
   // Return the promise that is returned by request
+  request('https://api.ipify.org/?format=json', (error, response, body))
+    .then(callback(null, JSON.parse(body).ip));
 };
 
 module.exports = { fetchMyIP };

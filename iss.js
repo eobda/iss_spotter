5f9550782169c13b70fd956d8ee9914f84ef78ad
maddie <lee.maddie@gmail.com>
2023-09-24 16:56:45 -0400

    Modularize printPassTimes function

--- iss.js
+++ iss.js
@@ -130,4 +130,24 @@ const nextISSTimesForMyLocation = function(callback) {
   });
 };
 
-module.exports = { nextISSTimesForMyLocation };
+/*
+Input:
+*   Array of data objects defining the next fly-overs of the ISS. [ { risetime: <number>, duration: <number> }, ... ]
+
+Returns:
+*   undefined
+
+Side effect:
+*   Console log messages to make that data more human readable.
+*    Example output: Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+*/
+const printPassTimes = function(passTimes) {
+  passTimes.forEach((passTime) => {
+    // convert epoch time date to human readable date
+    const date = new Date(passTime.risetime * 1000);
+    const duration = passTime.duration;
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation, printPassTimes };

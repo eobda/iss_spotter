d79f56e86c9a141003d5a8341a4142d0941a1901
maddie <lee.maddie@gmail.com>
2023-09-24 16:57:28 -0400

    Require printPassTimes from iss.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-const { nextISSTimesForMyLocation } = require('./iss');
-
-/*
-Input:
-*   Array of data objects defining the next fly-overs of the ISS. [ { risetime: <number>, duration: <number> }, ... ]
-
-Returns:
-*   undefined
-
-Side effect:
-*   Console log messages to make that data more human readable.
-*    Example output: Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
-*/
-const printPassTimes = function(passTimes) {
-  passTimes.forEach((passTime) => {
-    // convert epoch time date to human readable date
-    const date = new Date(passTime.risetime * 1000);
-    const duration = passTime.duration;
-    console.log(`Next pass at ${date} for ${duration} seconds!`);
-  });
-};
+const { nextISSTimesForMyLocation, printPassTimes } = require('./iss');
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
@@ -28,5 +8,3 @@ nextISSTimesForMyLocation((error, passTimes) => {
 
   printPassTimes(passTimes);
 });
-
-module.exports = { printPassTimes };

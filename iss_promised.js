98d03c29b9b23eb98358fb12095d9fa1ca372b70
maddie <lee.maddie@gmail.com>
2023-09-24 16:17:41 -0400

    Fix request promise syntax in fetchMyIP

--- iss_promised.js
+++ iss_promised.js
@@ -3,8 +3,7 @@ const request = require('request-promise-native');
 const fetchMyIP = function() {
   // Fetch the IP address from the API using request
   // Return the promise that is returned by request
-  request('https://api.ipify.org/?format=json', (error, response, body))
-    .then(JSON.parse(body).ip);
+  return request('https://api.ipify.org/?format=json');
 };
 
 module.exports = { fetchMyIP };

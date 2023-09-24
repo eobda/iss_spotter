fd11c1f1d4018c0cc12011c1442b85215ef752ba
maddie <lee.maddie@gmail.com>
2023-09-24 15:51:49 -0400

    Lint and comment code

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const { nextISSTimesForMyLocation } = require ('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
+/*
+Input:
+*   Array of data objects defining the next fly-overs of the ISS. [ { risetime: <number>, duration: <number> }, ... ]
+
+Returns:
+*   undefined
+
+Side effect:
+*   Console log messages to make that data more human readable.
+*    Example output: Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+*/
 const printPassTimes = function(passTimes) {
   passTimes.forEach((passTime) => {
     // convert epoch time date to human readable date
-    const date = new Date(passTime.risetime *1000);
+    const date = new Date(passTime.risetime * 1000);
     const duration = passTime.duration;
     console.log(`Next pass at ${date} for ${duration} seconds!`);
   });

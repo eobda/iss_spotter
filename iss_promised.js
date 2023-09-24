5d4f8429eb697849e0d0401cf26b3e2bdc7387ad
maddie <lee.maddie@gmail.com>
2023-09-24 16:24:17 -0400

    Define fetchCoordsByIP

--- iss_promised.js
+++ iss_promised.js
@@ -9,4 +9,13 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org/?format=json');
 };
 
+/*
+Makes a request to ipwho.is using the provided IP address to get its geographical information (latitude/longitude)
+-Input: JSON string containing the IP address
+-Returns: Promise of request for lat/lon
+*/
+const fetchCoordsByIP = function(body) {
+
+};
+
 module.exports = { fetchMyIP };

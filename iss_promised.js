ecd300ad3aff46d25d4a53c0347756bd81492832
maddie <lee.maddie@gmail.com>
2023-09-24 16:12:46 -0400

    Define fetchMyIP

--- iss_promised.js
+++ iss_promised.js
@@ -1,1 +1,6 @@
 const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  // Fetch the IP address from the API using request
+  // Return the promise that is returned by request
+};
